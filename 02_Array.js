07bbdeec04c254d9c506784121e90172c699b56f
Shravani <79784238+shravanivish@users.noreply.github.com>
2021-08-17 13:40:54 +0530

    Initial commit

---
+++ 02_Array.js
@@ -0,0 +1,8 @@
+// Find The Max & Min Element In An Array
+
+// Input: arr[] = { 1000, 11, 445, 1, 330, 3000 };
+// Output: Minimum element is 1
+//         Maximum element is 3000
+
+// Algorithm
+
