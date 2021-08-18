fbdbed36604db163053568e3f2a047b1b0829b2b
Shravani <79784238+shravanivish@users.noreply.github.com>
2021-08-18 20:46:41 +0530

    Initial commit

--- 02_Array.js
+++ 02_Array.js
@@ -1,8 +1,18 @@
 // Find The Max & Min Element In An Array
 
-// Input: arr[] = { 1000, 11, 445, 1, 330, 3000 };
+// Input: nums = [ 1000, 11, 445, 1, 330, 3000 ];
 // Output: Minimum element is 1
 //         Maximum element is 3000
 
 // Algorithm
 
+const nums = [5, 458, 120, -215, 228, 400, 122205, -85411];
+
+const max = (arr) => Math.max.apply(Math, arr);
+const min = (arr) => Math.min.apply(Math, arr);
+
+console.log("Maximum Element is", max(nums));
+console.log("Minimum Element is", min(nums));
+
+// Output: Maximum element is 122205
+//         Minimum element is -85411
