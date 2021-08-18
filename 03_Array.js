0c2311cde2d4dea438ea158d35ecb746475405ad
Shravani <79784238+shravanivish@users.noreply.github.com>
2021-08-18 21:18:05 +0530

    Initial commit

---
+++ 03_Array.js
@@ -0,0 +1,8 @@
+// Kth smallest element
+// Given an array and a number K where K is smaller than size of array, 
+// the task is to find the Kth smallest element in the given array 
+// It is given that all array elements are distinct
+// Input : nums = [7, 10, 4, 3, 20, 15] , K = 3
+// Output : Kth smallest element is 7
+
+// Algorithm
