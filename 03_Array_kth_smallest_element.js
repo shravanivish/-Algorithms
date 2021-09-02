f1365a0ace3139d296c7e26ede6a616665bfa191
Shravani <79784238+shravanivish@users.noreply.github.com>
2021-09-02 18:47:01 +0530

    Initial commit

--- 03_Array_kth_smallest_element.js
+++ 03_Array_kth_smallest_element.js
@@ -8,11 +8,11 @@
 
 // Algorithm
 const nums = [7, 10, 4, 3, 20, 15];
-const findKthSmallest = (array, k) => {
-	array.sort((a, b) => {
+const findKthSmallest = (nums, k) => {
+	nums.sort((a, b) => {
 		return a - b;
 	});
-	return array[k - 1];
+	return nums[k - 1];
 };
 console.log("Kth smallest element:", findKthSmallest(nums, 3));
 // Output : Kth smallest element: 7
